e2ac09bc4ba4102f96f0c055a586862604fd7a2f
Bùi Thanh Sơn <nitrozero2006@gmail.com>
2025-10-27 04:35:48 +0700

    import java.fx.scene.text.Font; import java.fx.scene.text.TextAligment;

--- BTL_Arkanoid/BTL_Arkanoid/src/main/java/arkanoid/GameManager.java
+++ BTL_Arkanoid/BTL_Arkanoid/src/main/java/arkanoid/GameManager.java
@@ -4,6 +4,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
